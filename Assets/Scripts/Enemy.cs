726ca4c4073eeb47c5c388f59871e3fbbc2229d2
Frank Mendez <frankmendezwebdev@gmail.com>
2020-03-23 10:03:58 +0800

    Player Lives & Damaging

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -28,6 +28,14 @@ public class Enemy : MonoBehaviour
     {
         if(other.tag == "Player")
         {
+            Player player = other.transform.GetComponent<Player>();
+
+
+            if (player != null)
+            {
+                player.Damage();
+            }
+
             Destroy(this.gameObject);
         }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,9 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _fireRate = 0.3f;
     private float _canFire = -1f;
+    [SerializeField]
+    private int _lives = 3;
+
 
     // Start is called before the first frame update
     void Start()
@@ -53,5 +56,15 @@ public class Player : MonoBehaviour
         _canFire = Time.time + _fireRate;
         Instantiate(_bulletPrefab, transform.position + new Vector3(-0.39f, 0.8f, 0), Quaternion.identity);
         Instantiate(_bulletPrefab, transform.position + new Vector3(0.39f, 0.8f, 0), Quaternion.identity);
+    }
+
+    public void Damage()
+    {
+        _lives -= 1;
+
+        if(_lives < 1)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
